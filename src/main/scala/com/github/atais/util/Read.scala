c2e5dff14563a720aa6335b9915830a8ea053084
Tobias Wennergren <wennergr@gmail.com>
2018-07-18 20:16:21 -0700

    Use standard functions from cats when converting from Try to Either
    Used standard cats functions for converting Try to Either and from
    Either to option

--- src/main/scala/com/github/atais/util/Read.scala
+++ src/main/scala/com/github/atais/util/Read.scala
@@ -1,6 +1,8 @@
 package com.github.atais.util
 
-import scala.util.Try
+import cats.implicits.catsSyntaxEitherObject
+import cats.implicits.catsSyntaxEither
+
 
 trait Read[A] extends Serializable {
 
@@ -16,42 +18,41 @@ object Read {
 
   // --------------------
   // implicits
-  import Util._
 
   implicit def optionRead[T](implicit r: Read[T]): Read[Option[T]] = create[Option[T]] {
     s => Right(r.read(s).toOption)
   }
 
   implicit val shortRead: Read[Short] = create[Short] {
-    s => Try(s.toShort).toEither
+    s => Either.catchNonFatal(s.toShort)
   }
 
   implicit val intRead: Read[Int] = create[Int] {
-    s => Try(s.toInt).toEither
+    s => Either.catchNonFatal(s.toInt)
   }
 
   implicit val longRead: Read[Long] = create[Long] {
-    s => Try(s.toLong).toEither
+    s => Either.catchNonFatal(s.toLong)
   }
 
   implicit val floatRead: Read[Float] = create[Float] {
-    s => Try(s.toFloat).toEither
+    s => Either.catchNonFatal(s.toFloat)
   }
 
   implicit val doubleRead: Read[Double] = create[Double] {
-    s => Try(s.toDouble).toEither
+    s => Either.catchNonFatal(s.toDouble)
   }
 
   implicit val booleanRead: Read[Boolean] = create[Boolean] {
-    s => Try(s.toBoolean).toEither
+    s => Either.catchNonFatal(s.toBoolean)
   }
 
   implicit val charRead: Read[Char] = create[Char] {
-    s => Try(s.head).toEither
+    s => Either.catchNonFatal(s.head)
   }
 
   implicit val stringRead: Read[String] = create[String] {
-    s => Try(s).toEither
+    s => Right(s)
   }
 }
 
